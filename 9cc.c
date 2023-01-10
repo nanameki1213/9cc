42de2130de6ade090fdba82aa30965391f458ff0
nanameki1213 <nanamekitaishin1213@icloud.com>
2023-01-10 15:42:43 +0900

    単項プラスと単項マイナスを追加

--- 9cc.c
+++ 9cc.c
@@ -46,6 +46,7 @@ struct Node {
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
@@ -161,6 +162,11 @@ Token *tokenize(char *p) {
   return head.next;
 }
 
+// expr    = mul ("+" mul | "-" mul)*
+// mul     = unary ("*" unary | "/" unary)*
+// unary   = ("+" | "-")? primary
+// primary = num | "(" expr ")"
+
 Node *expr() {
   Node *node = mul();
 
@@ -175,16 +181,24 @@ Node *expr() {
 }
 
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
 
   for(;;) {
     if(consume('*'))
-      node = new_node(ND_MUL, node, primary());
+      node = new_node(ND_MUL, node, unary());
     else if(consume('/'))
-      node = new_node(ND_DIV, node, primary());
+      node = new_node(ND_DIV, node, unary());
     else
       return node;
   }
+}
+
+Node *unary() {
+  if(consume('+'))
+    return primary();
+  if(consume('-'))
+    return new_node(ND_SUB, new_node_num(0), primary());
+  return primary();
 }
 
 Node *primary() {
